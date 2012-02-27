08b5898dbc77fd3f7ec85ead74ca2fb8ce5d2463
Ali Jafari <ali1dc@gmail.com>
2012-02-27 02:20:40 -0500

    init commit

---
+++ fb.js
@@ -0,0 +1,30 @@
+﻿/* Namespace */
+var MiniFireBugConsole = {};
+
+function runThisStatement(id) {
+    var script = document.getElementById(id).value;
+    var outputDiv = document.getElementById("fire-bug-output");
+    var result = outputDiv.innerHTML;
+    outputDiv.innerHTML = result + "<div class='new-line'>" + eval(script) + "</div>";
+
+    // for scrolling
+    outputDiv.scrollTop = outputDiv.scrollHeight;
+}
+
+function clearMiniFireBug() {
+    document.getElementById("fire-bug-output").innerHTML = "";
+}
+
+//http://www.randomsnippets.com/2008/02/12/how-to-hide-and-show-your-div/
+function toggle() {
+    var ele = document.getElementById("fire-bug-console");
+    var text = document.getElementById("toggle-my-firebug");
+    if (ele.style.display == "block") {
+        ele.style.display = "none";
+        text.value = "mini fire bug";
+    }
+    else {
+        ele.style.display = "block";
+        text.value = "Close";
+    }
+} 
