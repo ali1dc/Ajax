8daa9da15239fa441a9ac546b15eff28c406d355
Ali Jafari <ali1dc@gmail.com>
2012-02-29 00:28:43 -0500

    add logging

--- fb.js
+++ fb.js
@@ -1,12 +1,14 @@
 ﻿/* Namespace for avoiding name conflicts*/
 var MiniFireBugConsole = {};
 
-MiniFireBugConsole.executeScript = function() {
+MiniFireBugConsole.logs = new Array();
+
+MiniFireBugConsole.executeScript = function () {
     var outputDiv = document.getElementById("fire-bug-output");
     var script = document.getElementById("input-text-area").value;
     if (script == "") return;
-
     var scriptEval;
+    outputDiv.innerHTML += "<div class='input-command'><span>command>>> " + script + "</span></div>";
     try {
         // this will take care of global eval!
         //http://perfectionkills.com/global-eval-what-are-the-options/
@@ -14,17 +16,31 @@ MiniFireBugConsole.executeScript = function() {
         scriptEval = eval.call(this, script);
     }
     catch (err) {
-        scriptEval = "<span class='error'>" + err.message + "</span>";
+        scriptEval = "<span class='error'>error>>> " + err.message + "</span>";
     }
-    if (typeof scriptEval == "undefined") {
-        outputDiv.innerHTML += "<div class='input-command'><span> >>> " + script + "</span></div>";
-    }
-    else {
-        outputDiv.innerHTML += "<div class='input-command'><span> >>> " + script + "</span></div>";
+    if (typeof scriptEval != "undefined") {
+        //outputDiv.innerHTML += "<div class='input-command'><span> >>> " + script + "</span></div>";
         outputDiv.innerHTML += "<div class='new-line'>" + scriptEval + "</div>";
     }
     // for scrolling down to new output
     outputDiv.scrollTop = outputDiv.scrollHeight;
+}
+
+MiniFireBugConsole.log = function (input) {
+    if (document.getElementById("fire-bug-console").style.display == "none") {
+        MiniFireBugConsole.logs.push(input);
+    } else {
+        document.getElementById("fire-bug-output").innerHTML += "<div class='mini-firebug-log'><span>log>>> " + input + "</span></div>";
+    }
+}
+
+MiniFireBugConsole.printExistingLogs = function () {
+    var outputDiv = document.getElementById("fire-bug-output");
+    if (outputDiv != null) {
+        for (var i in MiniFireBugConsole.logs) {
+            outputDiv.innerHTML += "<div class='mini-firebug-log'><span>log>>> " + MiniFireBugConsole.logs[i] + "</span></div>";
+        }
+    }
 }
 
 // clear input command area
@@ -34,19 +50,22 @@ MiniFireBugConsole.clearMiniFireBug = function() {
 
 //http://www.randomsnippets.com/2008/02/12/how-to-hide-and-show-your-div/
 //for show and hide mini fire bug
-MiniFireBugConsole.toggle = function() {
+MiniFireBugConsole.toggle = function () {
     var ele = document.getElementById("fire-bug-console");
     var text = document.getElementById("toggle-my-firebug");
     if (ele.style.display == "block") {
         ele.style.display = "none";
         text.value = "mini fire bug";
+        // clear current history and logs // same as Firebug!
+        document.getElementById("fire-bug-output").innerHTML = "";
+        document.getElementById("input-text-area").value = "";
+        MiniFireBugConsole.logs = new Array();
     }
     else {
         ele.style.display = "block";
         text.value = "Close";
-        // clear current history
-        document.getElementById("fire-bug-output").innerHTML = "";
-        document.getElementById("input-text-area").value = "";
+        // print existing logs
+        MiniFireBugConsole.printExistingLogs()
     }
 }
 
